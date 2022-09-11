e0887f42c7ffde2ffef4f24503dc47531a7d4fa5
BOUARI ABDOUL HAKIM <bouariabdoulhakim@gmail.com>
2022-09-11 04:41:06 -0700

    3-print_alphabets.c update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,3 +13,15 @@ char upper_case;
 for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 {
 putchar (lower_case);
+}
+
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+putchar (upper_case);
+}
+
+putchar ("\n");
+
+return (0);
+
+}
