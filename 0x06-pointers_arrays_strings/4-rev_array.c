995a1ba83cbe776fbb934d83d3a5ea6b4368f501
BOUARI ABDOUL HAKIM <bouariabdoulhakim@gmail.com>
2022-09-21 15:50:51 -0700

    0x06. C - More pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+/**
+ * reverse_array - reverse the content
+ *@a: array of integers
+ *@n: number of elements
+ * Return: return void
+ */
+
+void reverse_array(int *a, int n)
+{
+int *start_c, *end_c, c;
+int i;
+
+
+
+start_c = a;
+end_c = a;
+
+for (i = 0; i < n - 1; i++)
+{
+end_c++;
+}
+
+
+for (i = 0; i < n / 2; i++)
+{
+
+c = *end_c;
+*end_c = *start_c;
+*start_c = c;
+
+start_c++;
+end_c--;
+}
+
+}
