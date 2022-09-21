ffdbc7e049beead9f03f2ff27200247faa0f70d5
BOUARI ABDOUL HAKIM <bouariabdoulhakim@gmail.com>
2022-09-21 16:22:38 -0700

    5-string_toupper.c first commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * string_toupper -> converting all char lowercase to uppercase
+ * @x: is string parameter
+ * Return: String
+ */
+char string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i])
+	{
+		if (x[i] >= 97 && x[i] <= 122
+			x[i] = x[i] - 32;
+		i++;
+	}
+	return (x);
+}	
