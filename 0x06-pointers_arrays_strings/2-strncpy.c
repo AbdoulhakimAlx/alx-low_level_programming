4113c5bdf5c0e7de5e52135ac45a606c5f71bad4
BOUARI ABDOUL HAKIM <bouariabdoulhakim@gmail.com>
2022-09-22 14:20:53 -0700

    2-strncpy.c commit 4

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _strncat - two words
+ * _strncpy - two words
  * @dest : pointer to char param
  * @src : pointer to char param
  * @n : int parameter
@@ -11,24 +11,6 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int m;
-	int i;
-
-	m = 0;
-
-	for (i = 0; i < 1000; i++)
-	{
-		if (dest[i] == '\0')
-		{
-			break;
-		}
-		m++;
-	}
-
-	for (i = 0; src[i] != '\0' && i < n; i++)
-	{
-		dest[m + i] = src[i];
-	}
-	dest[m + i] = '\0';
+	strncpy(dest, src, n);
 	return (dest);
 }
