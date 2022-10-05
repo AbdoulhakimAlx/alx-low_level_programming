3acec726fa85eec1a58153fd14092467db27748d
BOUARI ABDOUL HAKIM <bouariabdoulhakim@gmail.com>
2022-10-04 17:14:11 -0700

    0x0B. C - malloc, free commit two

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,7 +16,7 @@ char *_strdup(char *str)
 	if (str == NULL)
 		return (NULL);
 
-	for (i = 0; str[i] != '\0'; i++);
+	for (i = 0; str[i] != '\0'; i++)
 
 	a = malloc(i * sizeof(*a) + 1);
 	if (a == NULL)
