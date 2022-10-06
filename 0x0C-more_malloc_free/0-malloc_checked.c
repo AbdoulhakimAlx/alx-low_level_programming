a754a64a5b48a0cbcfed8dcbcc95c18029c0de2f
BOUARI ABDOUL HAKIM <bouariabdoulhakim@gmail.com>
2022-10-06 15:30:26 -0700

    0x0C. C - More malloc, free commit

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: bytes allocate
+ * Return: pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+void *pointer;
+
+pointer = malloc(b);
+if (pointer == NULL)
+exit(98);
+return (pointer);
+}
